58378a97c0e6494abf6f12c9a4301e4d7e40ca5c
Debopriyo Basu <debopriyobasu@gmail.com>
2023-11-30 23:37:37 +0530

    react & chakra setup

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme.jsx";
+import { BrowserRouter } from "react-router-dom";
+
+const rootElement = document.getElementById("root");
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
